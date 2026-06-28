87a9c380fac31467691cff6e6b16bf5b8b081095
hooghly85-sketch <hooghly85@gmail.com>
2026-06-28 10:33:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+// BRISTI PLAY - Script
 
+const player = document.getElementById("player");
+
+function playVideo(video){
+    player.src = video;
+    player.style.display = "block";
+
+    player.scrollIntoView({
+        behavior: "smooth"
+    });
+
+    player.play();
+}
+
+// Search (Part 1)
+const search = document.querySelector(".search");
+
+search.addEventListener("keyup", function(){
+
+    let value = this.value.toLowerCase();
+
+    let movies = document.querySelectorAll(".movie-card");
+
+    movies.forEach(function(movie){
+
+        let title = movie.querySelector("h3").innerText.toLowerCase();
+
+        if(title.includes(value)){
+            movie.style.display = "block";
+        }else{
+            movie.style.display = "none";
+        }
+
+    });
+
+});
